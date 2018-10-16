ec8814569bc7cb2623e7abbc22cef35f6109596d
Andy Haas <andy_haas@aol.com>
2018-10-16 08:56:55 -0500

    Update EditGroupRequest to lastest API Refrence info

--- RingCentral/Definitions/EditGroupRequest.cs
+++ RingCentral/Definitions/EditGroupRequest.cs
@@ -3,10 +3,8 @@ namespace RingCentral
     public partial class EditGroupRequest
     {
         // List of users to be added to a team
-        public string[] @addedPersonIds { get; set; }
-        // List of user email addresses to be added to a team (i.e. as guests)
-        public string[] @addedPersonEmails { get; set; }
+        public string[] @addedExtensionIds { get; set; }
         // List of users to be removed from a team
-        public string[] @removedPersonIds { get; set; }
+        public string[] @removedExtensionIds { get; set; }
     }
 }
